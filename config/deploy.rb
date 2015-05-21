444f4d0f32075e6ced43e94fc6d387ad4017acda
suezhen <sz3001@gmail.com>
2015-05-21 16:11:08 +0800

    add linked_dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ set :bundle_binstubs, nil
 set :linked_files, fetch(:linked_files, []).push('config/database.yml','config/boot_app.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('bin','log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 set :default_env, { path: "/opt/rubies/ruby-2.1.2/bin:$PATH" }
@@ -57,6 +57,6 @@ namespace :deploy do
   end
 
 
-  after :published, :restart_thin
+  # after :published, :restart_thin
 
 end
