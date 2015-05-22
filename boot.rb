76f9da02ca896531ed957a30c9e7e9d9fe65a17c
suezhen <sz3001@gmail.com>
2015-05-22 18:48:35 +0800

    valid token

--- boot.rb
+++ boot.rb
@@ -1,5 +1,20 @@
-  require "sinatra"
+require 'digest/sha1'
+require "sinatra"
 
-  get '/verify_token' do
-    'nandor'
-  end
+TOKEN = "nandor"
+
+get '/' do
+  params[:echostr]
+end
+
+before do
+  halt 401 unless valid?(params)
+end
+
+def valid?(params)
+  signature = params[:signature] || ''
+  timestamp = params[:timestamp] || ''
+  nonce = params[:nonce] || ''
+  Digest::SHA1.hexdigest([TOKEN, timestamp, nonce].sort.join) == signature ? true : false
+end
+
--- config/deploy.rb
+++ config/deploy.rb
@@ -36,10 +36,10 @@ set :bundle_binstubs, nil
 set :linked_files, fetch(:linked_files, []).push('config/database.yml','config/boot_app.yml')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('bin','log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
-set :default_env, { path: "/opt/rubies/ruby-2.1.2/bin:$PATH" }
+set :default_env, { path: "/opt/rubies/ruby-2.2.1/bin:$PATH" }
 
 
 # Default value for keep_releases is 5
